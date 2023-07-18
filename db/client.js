ebc0c60d977ca83a0d94d45645d67cb0c829263a
AmoghAmar23 <124466832+AmoghAmar23@users.noreply.github.com>
2023-07-18 17:54:33 -0500

    new

--- db/client.js
+++ db/client.js
@@ -14,7 +14,11 @@ const { Pool } = require('pg');
 
 //postgres://amogh:_2dmrWjVXmPAPzkXH98Dkxu7yhECXt6s0QD0H@dpg-cioo0fdph6elhbpdoltg-a.oregon-postgres.render.com/fitnessdev
 //postgres://amogh:WjVXmPAPzkXH98Dkxu7yhECXt6s0QD0H@dpg-cioo0fdph6elhbpdoltg-a.oregon-postgres.render.com/fitnessdev_2dmr
-const client = new Pool ({
+
+/*const client = new Pool (process.env.DATABASE_URL ? {
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : undefined,
+} : {
   host: 'dpg-cioo0fdph6elhbpdoltg-a.oregon-postgres.render.com',
   port: 5432,
   database: 'fitnessdev_2dmr',
@@ -24,7 +28,13 @@ const client = new Pool ({
   keepAliveInitialDelayMillis: 10000,
   ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : undefined,
 
+})*/
+
+const client = new Pool ({
+  connectionString: process.env.DATABASE_URL || 'https://postgres:Albino778899@localhost:5432/fitness-dev',
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : undefined,
 })
+
 // module.exports = {
 // client,
 // } 
